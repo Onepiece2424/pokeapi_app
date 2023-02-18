db7764542dc0068048500fbbe3abb0602612f511
rioto.nagano <kingdom950927@gmail.com>
2023-02-18 09:20:45 +0900

    ポケモンNo.で検索できる入力フォームの作成

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,7 +1,12 @@
 class PokemonsController < ApplicationController
   def index
-    raw_response = Faraday.get "https://pokeapi.co/api/v2/pokemon/pikachu"
-    @response = JSON.parse(raw_response.body)
+    return if params[:search].blank?
+      raw_response = Faraday.get "https://pokeapi.co/api/v2/pokemon/#{params[:search]}"
+    if raw_response.status == 200
+      @response = JSON.parse(raw_response.body)
+    else
+      redirect_to pokemons_path, notice: "#{raw_response.status}エラー！"
+    end
   end
 
   def new
