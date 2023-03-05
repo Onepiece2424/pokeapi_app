5bd383440931095bde15748657975f216a2ab10c
rioto.nagano <kingdom950927@gmail.com>
2023-03-05 09:54:16 +0900

    Itemインスタンスを生成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,12 @@
 class ItemsController < ApplicationController
   def new
+    return if params[:search].blank?
     raw_response = Faraday.get "https://pokeapi.co/api/v2/item/#{params[:search]}"
-
     if raw_response.status == 200
-      @item = JSON.parse(raw_response.body)
+      response = JSON.parse(raw_response.body)
+      @item = Item.new(item_id: response["id"], name: response["name"], image_url: response["sprites"]["default"])
     else
-      redirect_to new_pokemon_path
+      redirect_to root_path
     end
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+  validates :item_id, presence: true
+  validates :name, presence: true
+  validates :image_url, presence: true
 end
