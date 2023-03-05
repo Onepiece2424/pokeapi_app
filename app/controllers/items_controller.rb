ff05cabc43953277320fc99d59ef402d2fefc030
rioto.nagano <kingdom950927@gmail.com>
2023-03-05 09:16:52 +0900

    ポケモンアイテムの検索画面を作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,11 @@
 class ItemsController < ApplicationController
   def new
-    raw_response = Faraday.get "https://pokeapi.co/api/v2/item/100"
-    @response = JSON.parse(raw_response.body)
+    raw_response = Faraday.get "https://pokeapi.co/api/v2/item/#{params[:search]}"
+
+    if raw_response.status == 200
+      @item = JSON.parse(raw_response.body)
+    else
+      redirect_to new_pokemon_path
+    end
   end
 end
