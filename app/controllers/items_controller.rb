68973a6ff5f59d9d84076c6c7dda770858ca4962
rioto.nagano <kingdom950927@gmail.com>
2023-03-05 10:07:44 +0900

    Itemをゲットする機能を作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+  end
+
   def new
     return if params[:search].blank?
     raw_response = Faraday.get "https://pokeapi.co/api/v2/item/#{params[:search]}"
@@ -6,7 +11,23 @@ class ItemsController < ApplicationController
       response = JSON.parse(raw_response.body)
       @item = Item.new(item_id: response["id"], name: response["name"], image_url: response["sprites"]["default"])
     else
-      redirect_to root_path
+      redirect_to new_item_path, notice: "#{raw_response.status}エラー！"
     end
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to items_path, notice: "「#{@item.name}」をゲットしました。"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_id, :name, :image_url)
   end
 end
