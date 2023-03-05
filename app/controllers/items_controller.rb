7dbced793e26954ac6c263de592e19c7b794d405
rioto.nagano <kingdom950927@gmail.com>
2023-03-05 21:11:19 +0900

    不要な部分を削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,14 +4,6 @@ class ItemsController < ApplicationController
   end
 
   def new
-    # return if params[:search].blank?
-    # raw_response = Faraday.get "https://pokeapi.co/api/v2/item/#{params[:search]}"
-    # if raw_response.status == 200
-    #   response = JSON.parse(raw_response.body)
-    #   @item = Item.new(item_id: response["id"], name: response["name"], image_url: response["sprites"]["default"])
-    # else
-    #   redirect_to items_path, notice: "#{raw_response.status}エラー！"
-    # end
   end
 
   def create
--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -4,32 +4,6 @@ class PokemonsController < ApplicationController
   end
 
   def new
-    return if params[:search].blank?
-
-    raw_response = Faraday.get "https://pokeapi.co/api/v2/pokemon/#{params[:search]}"
-    if raw_response.status == 200
-      response = JSON.parse(raw_response.body)
-
-      File.open("pokemon.json") do |file|
-
-        # JSONファイルを読み込む
-        json = JSON.load(file)
-
-        # ポケモン名の先頭を大文字にする
-        keyword = response["name"].capitalize
-
-        # JSONファイルの中から検索
-        result = json.select { |x| x["en"].include?(keyword) }
-
-        # 変数に代入
-        val = result[0]["ja"]
-
-        # Pokemonインスタンスを生成
-        @pokemon = Pokemon.new(order: response["id"], name: val, image_url: response["sprites"]["front_default"])
-      end
-    else
-      redirect_to new_pokemon_path, notice: "#{raw_response.status}エラー！"
-    end
   end
 
   def create
